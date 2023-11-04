4987f24449fd7a738459841a3b6867630f20ba40
Hayri SAATCIOGLU <hayrisaatci@yandex.com>
2023-11-05 00:59:39 +0300

    server.js ön hazırlık

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+var cors = require("cors");
+const WebSocket = require("ws");
+const server = require("http").createServer(app);
+
+const wss = new WebSocket.Server({ server: server });
+wss.on("connection", function connection(ws) {
+  console.log("A new client Connected!");
+  //ws.send('Welcome New Client!');
+
+  ws.on("message", function incoming(message) {
+    //console.log('received: %s', message);
+
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(String(message));
+        //console.log('received---: %s', message);
+      }
+    });
+  });
+});
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+server.listen(3001, () => console.log(`Lisening on port :3001`));
